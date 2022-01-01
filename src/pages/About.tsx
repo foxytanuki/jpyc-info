664db345db36651826fe7dd9dfdc32c822dbc321
foxytanuki <biz@foxytanuki.dev>
2022-01-01 22:50:42 +0900

    Update About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -11,44 +11,33 @@ function About() {
     <PageWrapper>
       <ThemedBackground backgroundColor={backgroundColor} />
       <AutoColumn gap="lg">
-        <TYPE.largeHeader>JPYC Info (Unofficial)</TYPE.largeHeader>
+        <TYPE.largeHeader>JPYC Info</TYPE.largeHeader>
         <TYPE.black>
-          <TYPE.black>DEX全体のJPYC価格を表示するサイトです。</TYPE.black>
-          <TYPE.black>現在は以下3つのDEXに対応しています。</TYPE.black>
+          <TYPE.black>Polygon MainnetのJPYC価格キュレーションサイトです。</TYPE.black>
+          <TYPE.black>現在は以下2つのDEXに対応しています。</TYPE.black>
         </TYPE.black>
         <TYPE.blue>
           <TYPE.blue> - Uniswap V3</TYPE.blue>
-          <TYPE.blue> - Uniswap V2</TYPE.blue>
           <TYPE.blue> - Quickswap</TYPE.blue>
         </TYPE.blue>
-        <TYPE.black>以下のDEXは流動性が基準を超え次第対応します。</TYPE.black>
+        <TYPE.black>以下のDEXは順次気が向けば対応します。</TYPE.black>
         <TYPE.blue>
-          <TYPE.blue> - Sushiswap Ethereum mainnet</TYPE.blue>
-          <TYPE.blue> - Sushiswap Matic network</TYPE.blue>
+          <TYPE.blue> - Sushiswap</TYPE.blue>
         </TYPE.blue>
         <TYPE.black>
           <TYPE.black>万一、情報に誤りがあった場合においても制作者及び本サイトは一切の責任を負いかねます。</TYPE.black>
-          <TYPE.black>
-            特にUniswap V3についてはデータ取得先が開発中のため値に不具合が出る場合があります。ご注意ください。
-          </TYPE.black>
         </TYPE.black>
         <TYPE.black>
           <TYPE.black>フィードバック、ご意見、その他連絡についてはTwitter及びDiscordまでお願いします。</TYPE.black>
           <TYPE.black>こんな機能がほしい！というご意見も大歓迎です。</TYPE.black>
         </TYPE.black>
         <TYPE.darkGray>
-          <TYPE.darkGray>
-            <SocialIcon
-              network="twitter"
-              style={{ height: 24, width: 24, margin: 4 }}
-              url="https://twitter.com/foxyTanuki"
-            />
-            @foxyTanuki
-          </TYPE.darkGray>
-          <TYPE.darkGray>
-            <SocialIcon network="discord" style={{ height: 24, width: 24, margin: 4, marginRight: 8 }} />
-            foxytanuki#8096
-          </TYPE.darkGray>
+          <SocialIcon
+            network="twitter"
+            style={{ height: 24, width: 24, margin: 4 }}
+            url="https://twitter.com/foxyTanuki"
+          />
+          @foxyTanuki
         </TYPE.darkGray>
       </AutoColumn>
     </PageWrapper>
